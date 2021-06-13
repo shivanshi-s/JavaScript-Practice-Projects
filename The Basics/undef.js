979c647e2b2d8b74cfa38ebd20d0e62306c04d13
shivanshi <shivanshi.saxena2@gmail.com>
2021-06-13 16:39:13 +0530

    added files

---
+++ The Basics/undef.js
@@ -0,0 +1,14 @@
+var a;
+ 
+console.log(a);
+a=10;
+    console.log(a);
+
+a="hello world"
+console.log(a);
+// if(a === undefined){
+//     console.log("a is undefined");
+// }
+// else {
+//     console.log("a is not defined");
+// }
