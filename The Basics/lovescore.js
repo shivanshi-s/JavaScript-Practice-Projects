dda06f86d27afbd4ad5f116a8887b2f504c07f99
shivanshi <shivanshi.saxena2@gmail.com>
2021-06-22 21:30:15 +0530

    updated code

---
+++ The Basics/lovescore.js
@@ -0,0 +1,17 @@
+prompt("Enter your name :")
+prompt("Enter your lovers name :");
+
+var number = Math.random();
+number *=100;
+number = Math.floor(number)+1;
+alert("your lovescore is : "+number+"%");
+if (number>= 60)
+{
+    alert("NICE");
+}
+else 
+{
+    alert("Aw its ok love sucks anyway");
+}
+
+
