876f6aba7650965f74275bc31cdff6ac8fa30c1c
shivanshi <shivanshi.saxena2@gmail.com>
2021-08-17 16:20:49 +0530

    added scroll animationproj

---
+++ Project 8 - Scroll Animation/app.js
@@ -0,0 +1,16 @@
+const box =  document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight/5 * 4
+
+    box.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
