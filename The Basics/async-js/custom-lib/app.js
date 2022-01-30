127bfe07d7237c280765405a108af918c281303e
shivanshi <shivanshi.saxena2@gmail.com>
2022-01-30 18:05:31 +0530

    js learn

--- The Basics/async-js/custom-lib/app.js
+++ The Basics/async-js/custom-lib/app.js
@@ -9,3 +9,40 @@ function(err,posts){
       console.log(posts);
    }
 });
+
+// POST 
+//  create data
+const data = {
+   title : 'Custom Data',
+   body : 'this is a custom data'
+};
+
+// post req
+http.post('https://jsonplaceholder.typicode.com/posts',data,function(err,post){
+   if(err){
+      console.log(err);
+   }else {
+      console.log(post);
+   }
+});
+
+// Update the data 
+http.put('https://jsonplaceholder.typicode.com/posts/1',
+data,function(err,post){
+   if(err){
+      console.log(err);
+   }else {
+      console.log(post);
+   }
+});
+
+
+// delete req
+http.delete('https://jsonplaceholder.typicode.com/posts/1',
+function(err,response){
+   if(err) {
+      console.log(err);
+   } else {
+      console.log(response);
+   }
+});
